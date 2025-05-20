15a642855e39b8d9f6410fdcd55d703b080e0dee
roemu <20roman.suter02@gmail.com>
2025-05-20 17:43:24 +0200

    chore: update feedback-form

--- pkgs/feedback-form.nix
+++ pkgs/feedback-form.nix
@@ -1,22 +1,31 @@
-{buildGoModule, fetchFromGitHub, ...}: 
-
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+  ...
+}:
 buildGoModule rec {
-	pname = "feedback-form";
-	version = "1.1.4";
+  pname = "feedback-form";
+  version = "1.1.4";
 
-	src = fetchFromGitHub {
-	  owner = "roemu";
-	  repo = "feedback-form";
-	  rev = "v${version}";
-	  hash = "sha256-T1LDtSjkpUQWUwX86IqYalD5OkDrchXvjbwse62BD1U=";
-	};
-	
-	vendorHash = "sha256-EbxLmOtSweB/81GDLtzYLH9nrMVjWp5d72Wqws926us=";
+  src = fetchFromGitHub {
+    owner = "roemu";
+    repo = "feedback-form";
+    rev = "v${version}";
+    hash = "sha256-T1LDtSjkpUQWUwX86IqYalD5OkDrchXvjbwse62BD1U=";
+    # nix run nixpkgs#nix-prefetch-git -- https://github.com/roemu/feedback-form
+  };
 
-	env.CGO_ENABLED = 1;
+  vendorHash = "sha256-EbxLmOtSweB/81GDLtzYLH9nrMVjWp5d72Wqws926us=";
+  # Only changes if dependencies change
 
-	meta = {
-		description = "simple feedback form";
-		homepage = "https://github.com/roemu/feedback-form";
-	};
+  env.CGO_ENABLED = 1;
+
+  meta = {
+    description = "simple feedback form";
+    homepage = "https://github.com/roemu/feedback-form";
+    maintainers = with lib.maintainers; [
+      roemu
+    ];
+  };
 }
