a3fc9ee041ead79d5999ba9d2cd811371eff3064
U239087 <roman.suter2@sbb.ch>
2025-05-26 18:05:05 +0200

    fix(wofi): settings is not of type string

--- home/features/desktop/wofi.nix
+++ home/features/desktop/wofi.nix
@@ -7,19 +7,18 @@ in {
 	config = lib.mkIf cfg.enable {
 		programs.wofi = {
 			enable = true;
-			settings = ''
-				show=drun
-				width=750
-				height=400
-				always_parse_args=true
-				show_all=false
-				term=kitty
-				hide_scroll=true
-				print_command=true
-				insensitive=true
-				prompt=
-				columns=2
-			'';
+			settings = {
+				show="drun";
+				width=750;
+				height=400;
+				always_parse_args=true;
+				show_all=false;
+				term="ghostty";
+				hide_scroll=true;
+				print_command=true;
+				insensitive=true;
+				columns=2;
+			};
 			style = ''
 				@define-color	rosewater  #f5e0dc;
 				@define-color	rosewater-rgb  rgb(245, 224, 220);
