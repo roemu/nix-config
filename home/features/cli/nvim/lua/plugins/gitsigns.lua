7b84cc4093e3d9c373b121676a1147eab8267b4e
U239087 <roman.suter2@sbb.ch>
2025-06-01 13:07:55 +0200

    feat: update gitsigns and other non-breaking deps

--- home/features/cli/nvim/lua/plugins/gitsigns.lua
+++ home/features/cli/nvim/lua/plugins/gitsigns.lua
@@ -1,27 +1,16 @@
-
-  return {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
+return {
+	'lewis6991/gitsigns.nvim',
 	event = "BufEnter",
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', '<leader>gb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-      end,
-    },
-  }
+	keys = {
+		{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" },
+	},
+	opts = {
+		signs = {
+			add = { text = '+' },
+			change = { text = '~' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '~' },
+		},
+	},
+}
