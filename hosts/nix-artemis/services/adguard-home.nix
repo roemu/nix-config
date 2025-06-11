5a8145d918b76faefc958fa3279b0083af3532c8
roemu <20roman.suter02@gmail.com>
2025-06-11 07:43:09 +0200

    feat: enable adguard-home again, but leave out traefik exposure for now

--- hosts/nix-artemis/services/adguard-home.nix
+++ hosts/nix-artemis/services/adguard-home.nix
@@ -2,7 +2,29 @@
   services.adguardhome = {
     enable = true;
     openFirewall = true;
+    settings = {
+      http = {
+        address = "localhost:3000";
+      };
+    };
   };
-  networking.firewall.allowedTCPPorts = [80 53];
+  networking.firewall.allowedTCPPorts = [53 3000];
   networking.firewall.allowedUDPPorts = [53];
+
+  # services.traefik.dynamicConfigOptions.http = {
+  #   services.adguardhome.loadBalancer.servers = [
+  #     {
+  #       url = "http://localhost:3000/";
+  #     }
+  #   ];
+  #
+  #   routers.adguardhome = {
+  #     rule = "Host(`adguard.romansuter.ch`)";
+  #     tls = {
+  #       certResolver = "hetzner";
+  #     };
+  #     service = "adguardhome";
+  #     entrypoints = "websecure";
+  #   };
+  # };
 }
