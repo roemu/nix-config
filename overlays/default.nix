92292c2d08c78e771e1f01e52ee86c8c56a27607
U239087 <roman.suter2@sbb.ch>
2025-08-30 07:22:57 +0200

    chore(arc-browser): update version

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,10 +6,10 @@
 
   modifications = final: prev: {
     arc-browser = prev.arc-browser.overrideAttrs (old: rec {
-      version = "1.108.0-66882";
+      version = "1.110.0-67696";
       src = prev.fetchurl {
         url = "https://releases.arc.net/release/Arc-${version}.dmg";
-        hash = "sha256-L6GZv9COYgjzoMeU36nXVvyWCYUkXxW35lodeylZ2gE=";
+        hash = "sha256-Ce80v/eIcoGX3KTPV/0bKHnv3PqsvUfLE1o+kpZ/B4w=";
       };
       meta.knownVulerabilities = [];
     });
