88ccfbe0d3ea2604c7e7cb81cf800148581b3ce4
U239087 <roman.suter2@sbb.ch>
2025-09-19 08:28:59 +0200

    fix(ssh): defaultConfig will soon be deprecated

--- home/features/cli/ssh.nix
+++ home/features/cli/ssh.nix
@@ -9,6 +9,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     programs.ssh.enable = true;
+    programs.ssh.enableDefaultConfig = false; # Deprecated in the future, thus removed already
     programs.ssh.matchBlocks = {
       "nix-artemis" = {
         hostname = "100.64.0.1";
@@ -17,6 +18,18 @@ in {
       "nix-hephaestus" = {
         hostname = "100.64.0.2";
         user = "romansuter";
+      };
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
       };
     };
   };
