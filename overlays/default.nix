94957a131d9296839cdf0ac46df3d7d02e1c1f27
U239087 <roman.suter2@sbb.ch>
2026-01-09 09:06:29 +0100

    feat(overlays): add overlay for new vacuum version

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,6 +13,16 @@
     #   };
     #   meta.knownVulerabilities = [];
     # });
+    vacuum-go = prev.vacuum-go.overrideAttrs (old: rec {
+      version = "0.23.2";
+      src = prev.fetchFromGitHub {
+        owner = "daveshanley";
+        repo = "vacuum";
+        tag = "v${version}";
+        hash = "sha256-zQ9NzNPWJC431AF1wlrRAZRhUTMdKJO6qWa4x9jpFoE=";
+      };
+      vendorHash = "sha256-kpW1i6LJUFMJArSHYMI4taTfAcfDH+E39GOBOKZFu+c=";
+    });
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
