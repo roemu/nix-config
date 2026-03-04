5a1c00be8215998cbf0fb0ee2f4c9de29d8f9229
U239087 <roman.suter2@sbb.ch>
2026-03-04 11:01:51 +0100

    feat(nvim): update java config

--- home/features/cli/nvim/ftplugin/java.lua
+++ home/features/cli/nvim/ftplugin/java.lua
@@ -1,172 +1,161 @@
-local jdtls = require("jdtls")
-local jdtls_dap = require("jdtls.dap")
-local jdtls_setup = require("jdtls.setup")
-local home = os.getenv("HOME")
+local jdtls = require('jdtls');
+local jdtls_dap = require('jdtls.dap');
 
 vim.opt.tabstop = 4;
 vim.opt.shiftwidth = 4;
 vim.opt.expandtab = true;
 
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = jdtls_setup.find_root(root_markers)
+local jdtls_install = vim.fn.expand("$MASON/packages/jdtls")
+local lombok_path = jdtls_install .. "/lombok.jar"
 
-local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
-local workspace_dir = home .. "/.cache/jdtls/workspace" .. project_name
-local lsp = require("vim.lsp")
+local function jdtls_on_attach(_, _)
+	vim.keymap.set('n', '<leader>tc', ':lua require("jdtls").test_class()<CR>', { desc = '[T]est: [C]lass' })
+	vim.keymap.set('n', '<leader>tm', ':lua require("jdtls").test_nearest_method()<CR>',
+		{ desc = '[T]est: [M]ethod' })
+	vim.keymap.set('n', '<leader>tl', ':DapToggleRepl<CR>', { desc = "[T]oggle test result [L]ist" })
 
-local function mk_config()
-  local capabilities = lsp.protocol.make_client_capabilities()
-  capabilities.workspace.configuration = true
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  return {
-    flags = {
-      allow_incremental_sync = true,
-    },
-    handlers = {},
-    capabilities = capabilities,
-    init_options = {},
-    settings = {},
-  }
+	jdtls.setup_dap({ hotcodereplace = "auto" })
+	jdtls_dap.setup_dap_main_class_configs()
 end
 
-local function archConfigName()
-  local jit = require("jit")
-
-  if jit.os == 'OSX' and (jit.arch == 'arm64' or jit.arch == 'arm') then
-    return "config_mac_arm"
-  elseif jit.os == 'Linux' then
-    return "config_linux"
-  elseif jit.os == 'Windows' then
-    return "config_win"
-  end
+local function get_platform_config()
+	if vim.fn.has("mac") == 1 then
+		return jdtls_install .. "/config_mac"
+	elseif vim.fn.has("unix") == 1 then
+		return jdtls_install .. "/config_linux"
+	elseif vim.fn.has("win32") == 1 then
+		return jdtls_install .. "/config_win"
+	end
 end
 
-local config = mk_config();
-config.settings = {
-  java = {
-    referencesCodeLens = {
-      enabled = true,
-    },
-    references = {
-      includeDecompiledSources = true,
-    },
-    format = {
-      enabled = true,
-      settings = {
-        url = home .. "/.config/nvim/lua/lspconfig/intellij-java-google-style.xml",
-        profile = "GoogleStyle",
-      },
-    },
-    eclipse = {
-      downloadSources = true,
-    },
-    maven = {
-      downloadSources = true,
-    },
-    signatureHelp = { enabled = true },
-    contentProvider = { preferred = "fernflower" },
-    completion = {
-      favoriteStaticMembers = {
-        "org.mockito.ArgumentMatchers.*",
-        "org.junit.jupiter.api.Assertions.*",
-        "java.util.Objects.requireNonNull",
-        "java.util.Objects.requireNonNullElse",
-        "org.mockito.Mockito.*",
-      },
-      filteredTypes = {
-        "com.sun.*",
-        "io.micrometer.shaded.*",
-        "java.awt.*",
-        "jdk.*",
-        "sun.*",
-      },
-      importOrder = {
-        "com",
-        "org",
-        "java",
-        "javax",
-        "\\#",
-      },
-    },
-    sources = {
-      organizeImports = {
-        starThreshold = 9999,
-        staticStarThreshold = 9999,
-      },
-    },
-    codeGeneration = {
-      toString = {
-        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-      },
-      useBlocks = true,
-    },
-    configuration = {
-      runtimes = {
-        {
-          name = "JavaSE-21",
-          path = vim.fn.getenv("JAVA_HOME")
-        }
-      }
-    },
-    home = vim.fn.getenv("JAVA_HOME")
-  },
-}
-
-config.cmd = {
-  "java",
-
-  "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-  "-Dosgi.bundles.defaultStartLevel=4",
-  "-Declipse.product=org.eclipse.jdt.ls.core.product",
-  "-Dlog.protocol=true",
-  "-Dlog.level=ALL",
-  "-Xmx4g",
-  "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-  "--add-modules=ALL-SYSTEM",
-  "--add-opens", "java.base/java.util=ALL-UNNAMED",
-  "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-
-  "-jar", vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-  "-configuration", home .. "/.local/share/nvim/mason/packages/jdtls/" .. archConfigName(),
-  "-data", workspace_dir,
-}
-
-config.on_attach = function(_, _)
-  ---@diagnostic disable-next-line: missing-fields
-  jdtls.setup_dap({ hotcodereplace = "auto" })
-  jdtls_dap.setup_dap_main_class_configs()
-
-  vim.keymap.set('n', '<leader>tc', ':lua require("jdtls").test_class()<CR>', { desc = '[T]est: [C]lass' })
-  vim.keymap.set('n', '<leader>tm', ':lua require("jdtls").test_nearest_method()<CR>',
-    { desc = '[T]est: [M]ethod' })
-  vim.keymap.set('n', '<leader>tl', ':DapToggleRepl<CR>', { desc = "[T]oggle test result [L]ist" })
+local function get_data_dir()
+	local jdtls_dir = vim.fn.stdpath("cache") .. "/nvim-jdtls"
+	local data_dir = jdtls_dir .. "/" .. string.gsub(vim.fn.getcwd(), "/", "_")
+	return data_dir
 end
 
-config.on_init = function(client, _)
-  if client.config.settings then
-    client:notify('workspace/didChangeConfiguration', { settings = client.config.settings })
-  end
+local function getDebugAndTestBundles()
+	local java_debug_path = vim.fn.expand("$MASON/packages/java-debug-adapter")
+	local java_test_path = vim.fn.expand("$MASON/packages/java-test")
+	local bundles = {
+		vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true),
+	}
+	vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", true), "\n"))
+	return bundles
 end
 
-local extendedClientCapabilities = require("jdtls").extendedClientCapabilities
-extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+local config = {
+	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+	on_attach = jdtls_on_attach,
+	capabilities = require('blink.cmp').get_lsp_capabilities({
+		textDocument = { completion = { completionItem = { snippetSupport = true } } },
+	}),
 
-local bundles = {
-  vim.fn.glob(home ..
-  "/.local/share/java/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar")
-}
-vim.list_extend(bundles, vim.split(vim.fn.glob(home .. '/.local/share/java/vscode-java-test/server/*.jar'), "\n"))
-bundles = vim.tbl_filter(function(bundle)
-  return bundle ~= "com.microsoft.java.test.runner-jar-with-dependencies.jar" and bundle ~= "jacocoagent.jar"
-end, bundles)
+	cmd = {
+		'java',
+		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+		'-Dosgi.bundles.defaultStartLevel=4',
+		'-Declipse.product=org.eclipse.jdt.ls.core.product',
+		'-Dlog.protocol=true',
+		'-Dlog.level=ALL',
+		'-Xmx1g',
+		"-javaagent:" .. lombok_path,
+		"-Xmx12G",
+		'--add-modules=ALL-SYSTEM',
+		'--add-opens',
+		'java.base/java.util=ALL-UNNAMED',
+		'--add-opens',
+		'java.base/java.lang=ALL-UNNAMED',
 
-config.init_options = {
-  bundles = bundles,
-  extendedClientCapabilities = extendedClientCapabilities,
-}
+		"-jar",
+		vim.fn.glob(jdtls_install .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
 
-config.filetypes = {
-  "java"
+		'-configuration',
+		get_platform_config(),
+
+		'-data',
+		get_data_dir(),
+	},
+
+	root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+
+	-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+	settings = {
+		java = {
+			autobuild = {
+				enabled = false
+			},
+			eclipse = {
+				downloadSources = true,
+			},
+			configuration = {
+				updateBuildConfiguration = "disabled",
+			},
+			maven = {
+				downloadSources = true,
+			},
+			format = {
+				enabled = true,
+				settings = {
+					url = vim.fn.expand("~/.config/nvim/lua/lspconfig/intellij-java-google-style.xml"),
+					profile = "GoogleStyle",
+				},
+			},
+			maxConcurrentBuilds = 2,
+			jdt = {
+				ls = {
+					javac = {
+						enabled = "false"
+					}
+				}
+			},
+			signatureHelp = {
+				enabled = true,
+			},
+			edit = {
+				smartSemicolonDetection = {
+					enabled = true,
+				}
+			},
+			saveActions = {
+				cleanup = false
+			},
+			completion = {
+				maxResults = 20,
+				engine = "dom",
+				favoriteStaticMembers = {
+					"org.hamcrest.MatcherAssert.assertThat",
+					"org.hamcrest.Matchers.*",
+					"org.hamcrest.CoreMatchers.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"java.util.Objects.requireNonNull",
+					"java.util.Objects.requireNonNullElse",
+					"org.mockito.Mockito.*",
+				},
+				filteredTypes = {
+					"com.sun.*",
+					"io.micrometer.shaded.*",
+					"java.awt.*",
+					"jdk.*",
+					"sun.*",
+				},
+				matchCase = "off",
+				guessMethodArguments = "insertBestGuessedArguments",
+			},
+			contentProvider = {
+				preferred = "fernflower",
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			implementationsCodeLens = {
+				enabled = true
+			}
+		}
+	},
+
+	init_options = {
+		bundles = getDebugAndTestBundles(),
+	},
 }
--- Start Server
 require('jdtls').start_or_attach(config)
