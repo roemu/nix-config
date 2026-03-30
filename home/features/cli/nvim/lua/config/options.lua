b5720cb34ec723504abbb017b8919e36173c9c31
U239087 <roman.suter2@sbb.ch>
2026-03-30 08:15:58 +0200

    fix(nvim): tabstop not at recommended value

--- home/features/cli/nvim/lua/config/options.lua
+++ home/features/cli/nvim/lua/config/options.lua
@@ -9,7 +9,7 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 10
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
