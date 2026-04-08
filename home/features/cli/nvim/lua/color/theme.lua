ed84c3a2a0b1a10b7c6dad85780c854fe5cd3491
U239087 <roman.suter2@sbb.ch>
2026-04-08 07:27:21 +0200

    feat(nvim): update colorscheme auto_integration and java codelens

--- home/features/cli/nvim/lua/color/theme.lua
+++ home/features/cli/nvim/lua/color/theme.lua
@@ -1,32 +1,28 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		lazy = false,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				integrations = {
-					mason = true,
-					which_key = true,
-				},
-			})
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                auto_integrations = true,
+            })
 
-			vim.cmd.colorscheme "catppuccin"
-			vim.api.nvim_set_hl(0, 'LineNr', { fg = '#c0c0c0' })
-		end,
-	},
-	{
-		'nvim-lualine/lualine.nvim',
-		event = "VeryLazy",
-		opts = {
-			options = {
-				icons_enabled = false,
-				theme = 'catppuccin',
-				component_separators = '|',
-				section_separators = { right = '', left = '' },
-			},
-		},
-	},
+            vim.cmd.colorscheme "catppuccin-nvim"
+        end,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        event = "VeryLazy",
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'catppuccin-nvim',
+                component_separators = '|',
+                section_separators = { right = '', left = '' },
+            },
+        },
+    },
 }
--- home/features/cli/nvim/lua/config/autocmds.lua
+++ home/features/cli/nvim/lua/config/autocmds.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd('FileType', {
 
 vim.cmd(':au BufRead,BufEnter *.component.html set filetype=htmlangular')
 
-vim.api.nvim_create_autocmd({ 'BufWritePost', 'LspAttach' }, {
+vim.api.nvim_create_autocmd({ 'LspAttach' }, {
 	pattern = { "*.java" },
 	callback = function()
 		local _, _ = pcall(vim.lsp.codelens.refresh)
