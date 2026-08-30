d154b6b124ce59034a479bc8d9712147732c4400
roemu <20roman.suter02@gmail.com>
2026-08-30 15:32:41 +0200

    chore(deps): update lazy-lock

--- home/features/cli/nvim/lua/lspconfig/lspconfig.lua
+++ home/features/cli/nvim/lua/lspconfig/lspconfig.lua
@@ -1,50 +1,52 @@
 vim.lsp.config.lua_ls = {
-	settings = {
-		Lua = {
-			runtime = {
-				version = 'LuaJIT',
-			},
-			diagnostics = {
-				globals = { 'vim' },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-		}
-	}
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    }
+  }
 }
 
 vim.lsp.config.helm_ls = {
-	filetypes = { 'helm', 'helmfile' },
+  filetypes = { 'helm', 'helmfile' },
 }
 
 vim.lsp.config.ts_ls = {
-	filetypes = { 'ts', 'typescript' },
+  filetypes = { 'ts', 'typescript' },
 }
 vim.lsp.config.clangd = {
-	filetypes = { 'arduino', 'c', 'cpp' },
+  filetypes = { 'arduino', 'c', 'cpp' },
 }
 
 vim.lsp.config('*', {
-	root_markers = { '.git' },
-	capabilities = vim.lsp.protocol.make_client_capabilities(),
+  root_markers = { '.git' },
+  capabilities = vim.lsp.protocol.make_client_capabilities(),
 })
 vim.lsp.enable({ 'emmet_ls', 'bashls', 'gopls', 'openscad_lsp', 'yamlls', 'helm_ls', 'clangd', 'nil_ls', 'lua_ls',
-	'ts_ls', 'angularls', 'rust_analyzer', 'vacuum', 'jsonls' })
+  'ts_ls', 'angularls', 'rust_analyzer', 'vacuum', 'jsonls' })
 
 return {
-	'mason-org/mason-lspconfig.nvim',
-	opts = {
-		ensure_installed = {
-			jdtls = { filetypes = { 'java' } }
-		},
-		automatic_enable = false,
-	},
-	dependencies = {
-		{ 'mason-org/mason.nvim', opts = {} },
-		{
-			'neovim/nvim-lspconfig',
-		},
-		{ 'j-hui/fidget.nvim',    lazy = true, opts = {} },
-	},
+  {
+    'mason-org/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = {
+        jdtls = { filetypes = { 'java' } }
+      },
+      automatic_enable = false,
+    },
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {} },
+      {
+        'neovim/nvim-lspconfig',
+      },
+      { 'j-hui/fidget.nvim',    lazy = true, opts = {} },
+    },
+  }
 }
